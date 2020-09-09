525ca55367e83b30ff2335995b8659b2e204157e
oskarskompski <71001469+oskarskompski@users.noreply.github.com>
2020-09-09 09:34:13 +0200

    Add files via upload
    pierwsza lekcja :)

---
+++ PAI/1_podstawy.php
@@ -0,0 +1,9 @@
+<?php
+  $name = 'Jarek';
+  $lastname = 'Pojarek';
+  echo 'imię: $name<br>';
+  echo "imię: $name<br>";
+  // konkatenacja .
+  echo " nazwisko: $lastname".'<br>';
+  echo 'jakby nie bylo jest bardzo milo'
+ ?>
